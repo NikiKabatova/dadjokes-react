33e9af6d867cf6b6ba0602bed2b97009733e2c49
Nikola Kabátová <niki.kabatova@gmail.com>
2022-05-28 16:09:01 +0200

    create component Joke, trying communication with parent aka at this point im lostaf

---
+++ src/Joke/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  return (
+    <div className="joke">
+      <div className="joke__body">
+        <div className="joke__user">
+          <img className="user-avatar" src={userAvatar} />
+          <p className="user-name">{userName}</p>
+        </div>
+        <p className="joke__text">{text}</p>
+      </div>
+      <div className="joke__likes">
+        <button
+          onClick={() => setLikes(likes + 1)}
+          id="btn-up"
+          className="btn-like btn-like--up"
+        ></button>
+        <span id="likes-up" className="likes-count likes-count--up">
+          {likes}
+        </span>
+        <button
+          onClick={() => setDislikes(dislikes + 1)}
+          id="btn-down"
+          className="btn-like btn-like--down"
+        ></button>
+        <span id="likes-down" className="likes-count likes-count--down">
+          {dislikes}
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -2,48 +2,32 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
 import './style.css';
+import { Joke } from './Joke/INDEX';
+
+const jokes = [
+  {
+    id: 'user01',
+    avatar:
+      'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png',
+    name: 'Neroxx',
+    text: 'The secret service isn\'t allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"',
+    likes: 13,
+    dislikes: 24,
+  },
+];
 
 const App = () => {
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
-  return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
 
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            onClick={() => setLikes(likes + 1)}
-            id="btn-up"
-            className="btn-like btn-like--up"
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likes}
-          </span>
-          <button
-            onClick={() => setDislikes(dislikes + 1)}
-            id="btn-down"
-            className="btn-like btn-like--down"
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislikes}
-          </span>
-        </div>
+  return (
+    <>
+      <div className="container">
+        {jokes.map((item) => (
+          <Joke userName={item.userName} />
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
