799d6d9cbb6d03ed23f5cdcb17e65b9a81d32681
Nikola Kabátová <niki.kabatova@gmail.com>
2022-05-28 11:45:20 +0200

    add usestate + add  setLikes onClick for btn-like--upp

--- src/index.jsx
+++ src/index.jsx
@@ -1,49 +1,51 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { useState } from 'react';
 import './style.css';
 
-const App = () => (
-  <div className="container">
-    <div className="joke">
-      <div className="joke__body">
-        <div className="joke__user">
-          <img
-            className="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
-        </div>
+const App = () => {
+  const [likes, setLikes] = useState(0);
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
 
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
-      </div>
-      <div className="joke__likes">
-        <button
-          onClick={() => {
-            console.log('like');
-          }}
-          id="btn-up"
-          className="btn-like btn-like--up"
-        ></button>
-        <span id="likes-up" className="likes-count likes-count--up">
-          0
-        </span>
-        <button
-          onClick={() => {
-            console.log('dis-like');
-          }}
-          id="btn-down"
-          className="btn-like btn-like--down"
-        ></button>
-        <span id="likes-down" className="likes-count likes-count--down">
-          0
-        </span>
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            onClick={() => setLikes(likes + 1)}
+            id="btn-up"
+            className="btn-like btn-like--up"
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likes}
+          </span>
+          <button
+            onClick={() => {
+              console.log('dis-like');
+            }}
+            id="btn-down"
+            className="btn-like btn-like--down"
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            0
+          </span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 createRoot(document.querySelector('#app')).render(<App />);
