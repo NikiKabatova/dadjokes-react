4d76bb0bd5550ef048b41469163bb9e93d278d76
Nikola Kabátová <niki.kabatova@gmail.com>
2022-05-28 17:37:23 +0200

    add handleLikes/handleDislikes

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -9,24 +9,28 @@ export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
           <p className="user-name">{userName}</p>
         </div>
         <p className="joke__text">{text}</p>
-      </div>
-      <div className="joke__likes">
-        <button
-          onClick={() => setLike(like + 1)}
-          id="btn-up"
-          className="btn-like btn-like--up"
-        ></button>
-        <span id="likes-up" className="likes-count likes-count--up">
-          {like}
-        </span>
-        <button
-          onClick={() => setDislike(dislike + 1)}
-          id="btn-down"
-          className="btn-like btn-like--down"
-        ></button>
-        <span id="likes-down" className="likes-count likes-count--down">
-          {dislike}
-        </span>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => {
+              likes();
+            }}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {likes}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => {
+              dislikes();
+            }}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {dislikes}
+          </span>
+        </div>
       </div>
     </div>
   );
