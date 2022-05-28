9c0d43a8a716da1f9a47344c48da5ce6aae34931
Nikola Kabátová <niki.kabatova@gmail.com>
2022-05-28 11:36:15 +0200

    add onClick - test with console.log + change class to className

--- src/index.jsx
+++ src/index.jsx
@@ -3,30 +3,42 @@ import { createRoot } from 'react-dom/client';
 import './style.css';
 
 const App = () => (
-  <div class="container">
-    <div class="joke">
-      <div class="joke__body">
-        <div class="joke__user">
+  <div className="container">
+    <div className="joke">
+      <div className="joke__body">
+        <div className="joke__user">
           <img
-            class="user-avatar"
+            className="user-avatar"
             src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
           />
-          <p class="user-name">Neroxx</p>
+          <p className="user-name">Neroxx</p>
         </div>
 
-        <p class="joke__text">
+        <p className="joke__text">
           The secret service isn't allowed to yell "Get down!" anymore when the
           president is about to be attacked. Now they have to yell "Donald,
           duck!"
         </p>
       </div>
-      <div class="joke__likes">
-        <button id="btn-up" class="btn-like btn-like--up"></button>
-        <span id="likes-up" class="likes-count likes-count--up">
+      <div className="joke__likes">
+        <button
+          onClick={() => {
+            console.log('like');
+          }}
+          id="btn-up"
+          className="btn-like btn-like--up"
+        ></button>
+        <span id="likes-up" className="likes-count likes-count--up">
           0
         </span>
-        <button id="btn-down" class="btn-like btn-like--down"></button>
-        <span id="likes-down" class="likes-count likes-count--down">
+        <button
+          onClick={() => {
+            console.log('dis-like');
+          }}
+          id="btn-down"
+          className="btn-like btn-like--down"
+        ></button>
+        <span id="likes-down" className="likes-count likes-count--down">
           0
         </span>
       </div>
